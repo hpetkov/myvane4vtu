b35c8b8549410d74fd28c0a3c408c9617e5ea22e
Hrisian petkov <hrisian.petkov@gmail.com>
2015-11-14 11:21:05 +0200

    Main

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->string('city', 255);
+            $table->string('country', 255);
+            $table->string('address', 255);
+            $table->string('mobile_number', 255);
+            $table->string('email')->unique();
+            $table->string('ip');
+            $table->string('password', 60);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
